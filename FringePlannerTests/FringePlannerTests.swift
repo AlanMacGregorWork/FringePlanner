af0824059ea4773b4442f89ea7dbb87b3b5ab8b5
Alan MacGregor <alan.macgregor+work@gmail.com>
2024-09-18 21:18:54 +0100

    WIP: Test cases of different types

--- FringePlannerTests/FringePlannerTests.swift
+++ FringePlannerTests/FringePlannerTests.swift
@@ -5,6 +5,7 @@
 //  Created by Alan MacGregor on 16/09/2024.
 //
 
+import Foundation
 import Testing
 @testable import FringePlanner
 
@@ -13,5 +14,28 @@ struct FringePlannerTests {
     @Test func testExample() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
+    
+    @Test func doAnotherTest() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    
+    @Test func failureTest() throws {
+        try #require(Date.now == Date.now.addingTimeInterval(34))
+    }
 
+}
+
+
+import XCTest
+
+final class DeepLinkHandlerTests: XCTestCase {
+    
+    func testValidateURLShouldContinueInApp() {
+        
+    }
+    
+    func testOther() {
+        XCTFail("Failure message here this test")
+    }
 }
