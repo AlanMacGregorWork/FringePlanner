a561d4d0a6cc689054ac68260f1eb6cf45280141
Alan MacGregor <alan.macgregor+work@gmail.com>
2024-10-06 20:12:52 +0100

    Added ViewDataProtocol

---
+++ FringePlanner/Protocol/ViewDataProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  ViewDataProtocol.swift
+//  FringePlanner
+//
+//  Created by Alan MacGregor on 06/10/2024.
+//
+
+import SwiftUI
+
+/// Defines a type that can contain the data for a view
+protocol ViewDataProtocol {
+    /// The view that can be initialised with the data in this model
+    associatedtype ContentView: ViewProtocol where ContentView.DataType == Self
+}
+
+/// Defines a view that can be generated from a corresponding data model
+protocol ViewProtocol: View {
+    /// The data model required to build the view
+    associatedtype DataType: ViewDataProtocol
+    init(data: DataType)
+}
