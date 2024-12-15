cff0844b04f54cb41a41f880cd98b862c9813bb5
Alan MacGregor <alan.macgregor+work@gmail.com>
2024-12-15 16:26:48 +0000

    Removed colorScheme setup from AttributedString

--- FringePlanner/Extension/AttributedString.swift
+++ FringePlanner/Extension/AttributedString.swift
@@ -11,10 +11,9 @@ extension AttributedString {
     /// Create an attributed string from HTML.
     /// - Parameters:
     ///   - html: The HTML string to convert (may also be a standard string)
-    ///   - colorScheme: The color scheme to use for the attributed string.
     /// - Returns: An attributed string created from the HTML.
-    init?(from html: String, colorScheme: ColorScheme) {
-        let styledHTML = Self.createHTML(fromString: html, colorScheme: colorScheme)
+    init?(from html: String) {
+        let styledHTML = Self.createHTML(fromString: html)
         let data = Data(styledHTML.utf8)
         do {
             let nsAttributedString = try NSAttributedString(
@@ -31,13 +30,13 @@ extension AttributedString {
         }
     }
     
-    private static func createHTML(fromString string: String, colorScheme: ColorScheme) -> String {
+    private static func createHTML(fromString string: String) -> String {
         """
         <style>
             body {
                 font-family: -apple-system; /* Due the default Apple font */
                 font-size: \(UIFont.preferredFont(forTextStyle: .body).pointSize)px; /* Use the system font size */
-                color: \(colorScheme == .dark ? "white" : "black"); /* Add support for dark mode */
+                color: \(hexString(from: .gray)); /* Use the system color (which appears to be gray for both modes) */
             }
             p:last-child {
                 display: inline; /* Ensure that the last paragraph does not add a blank new line at the end */
@@ -47,5 +46,18 @@ extension AttributedString {
             \(string)
         </body>
         """
+    }
+    
+    // Convert UIColor to hex string
+    private static func hexString(from color: Color) -> String {
+        let uiColor = UIColor(color)
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        return String(format: "#%02X%02X%02X", Int(red * 255), Int(green * 255), Int(blue * 255))
     }
 }
