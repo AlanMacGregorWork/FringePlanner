f21ba47dbf9525284011fa3feb9332a2a1ab995f
Alan MacGregor <alan.macgregor+work@gmail.com>
2024-12-31 13:10:38 +0000

    FringePerformanceType now conforms to Codable

--- FringePlanner/API/Models/Enum/FringePerformanceType.swift
+++ FringePlanner/API/Models/Enum/FringePerformanceType.swift
@@ -6,20 +6,39 @@
 //
 
 /// Defines how the performance can experienced
-enum FringePerformanceType: Decodable, Equatable, Hashable {
+enum FringePerformanceType: Equatable, Hashable {
     case inPerson
     case onlineLive
     case onlineOnDemand
     case other(String)
+}
+
+// MARK: Codable
+
+private let kInPerson = "in-person"
+private let kOnlineLive = "online-live"
+private let kOnlineOnDemand = "online-on-demand"
+
+extension FringePerformanceType: Codable {
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        let stringValue = switch self {
+        case .inPerson: kInPerson
+        case .onlineLive: kOnlineLive
+        case .onlineOnDemand: kOnlineOnDemand
+        case .other(let value): value
+        }
+        try container.encode(stringValue)
+    }
     
     init(from decoder: any Decoder) throws {
         let container = try decoder.singleValueContainer()
         let stringValue = try container.decode(String.self).lowercased()
         
         switch stringValue {
-        case "in-person": self = .inPerson
-        case "online-live": self = .onlineLive
-        case "online-on-demand": self = .onlineOnDemand
+        case kInPerson: self = .inPerson
+        case kOnlineLive: self = .onlineLive
+        case kOnlineOnDemand: self = .onlineOnDemand
         default:
             fringeAssertFailure("Found non-identified performance type")
             self = .other(stringValue.trimmed)
---
+++ FringePlannerTests/APITests/FringeAPICodableTests.swift
@@ -0,0 +1,43 @@
+//
+//  FringeAPICodableTests.swift
+//  FringePlanner
+//
+//  Created by Alan MacGregor on 31/12/2024.
+//
+
+import Testing
+import Foundation
+@testable import FringePlanner
+
+@Suite("Fringe API Codable Tests")
+struct FringeAPICodableTests {
+    @Test("FringePerformanceType")
+    func testFringePerformanceType() throws {
+        try testEncodeDecodeEquates(value: FringePerformanceType.inPerson)
+        try testEncodeDecodeEquates(value: FringePerformanceType.onlineLive)
+        try testEncodeDecodeEquates(value: FringePerformanceType.onlineOnDemand)
+        try testEncodeDecodeEquates(value: FringePerformanceType.other("some alternative value"))
+    }
+}
+
+// MARK: - Helpers
+
+/// Encodes & decodes the value, then verifies the newly decoded value matches the original value
+private func testEncodeDecodeEquates<T: Encodable & Decodable & Equatable>(
+    value: T,
+    sourceLocation: SourceLocation = #_sourceLocation
+) throws {
+    do {
+        let encodedData = try JSONEncoder().encode(value)
+        
+        // Decoding for DB
+        let decodedValueDB = try JSONDecoder().decode(T.self, from: encodedData)
+        #expect(value == decodedValueDB, sourceLocation: sourceLocation)
+        
+        // Decoding for JSON
+        let decodedValueJSON = try fringeJsonDecoder.decode(T.self, from: encodedData)
+        #expect(value == decodedValueJSON, sourceLocation: sourceLocation)
+    } catch {
+        Issue.record(error, sourceLocation: sourceLocation)
+    }
+}
