cc44a05ea2cefc40af51ec58d2a367230f62908e
Alan MacGregor <alan.macgregor+work@gmail.com>
2025-01-23 22:49:24 +0000

    FringePerformanceType, FringePriceType, & FringeStatus now support RawRepresentable for improved use of SwiftData

--- FringePlanner/API/Models/Enum/FringePriceType.swift
+++ FringePlanner/API/Models/Enum/FringePriceType.swift
@@ -15,33 +15,46 @@ enum FringePriceType: Equatable, Hashable {
 
 // MARK: Codable
 
-private let kPaid = "paid"
-private let kFree = "free"
-private let kPayWhatYouCan = "pay-what-you-can"
+// Note: Codable is predominantly used from decoding the JSON from the API however SwiftData has issues with this
+// so relies on the `RawRepresentable` for its reading & writing
 
 extension FringePriceType: Codable {
     func encode(to encoder: any Encoder) throws {
         var container = encoder.singleValueContainer()
-        let stringValue = switch self {
-        case .paid: kPaid
-        case .free: kFree
-        case .payWhatYouCan: kPayWhatYouCan
-        case .other(let value): value
-        }
-        try container.encode(stringValue)
+        try container.encode(rawValue)
     }
     
     init(from decoder: any Decoder) throws {
         let container = try decoder.singleValueContainer()
         let stringValue = try container.decode(String.self)
-        
-        switch stringValue {
+        self = .init(rawValue: stringValue) ?? .other(stringValue)
+    }
+}
+
+// MARK: RawRepresentable
+
+private let kPaid = "paid"
+private let kFree = "free"
+private let kPayWhatYouCan = "pay-what-you-can"
+
+extension FringePriceType: RawRepresentable {
+    init?(rawValue: String) {
+        switch rawValue {
         case kPaid: self = .paid
         case kFree: self = .free
         case kPayWhatYouCan: self = .payWhatYouCan
         default:
             fringeAssertFailure("Found non-identified price type")
-            self = .other(stringValue.trimmed)
+            self = .other(rawValue.trimmed)
+        }
+    }
+    
+    var rawValue: String {
+        switch self {
+        case .paid: return kPaid
+        case .free: return kFree
+        case .payWhatYouCan: return kPayWhatYouCan
+        case .other(let value): return value
         }
     }
 }
