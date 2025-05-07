7d8a4985fa076f066954fcac9290f9b9246bd3f9
Alan MacGregor <alan.macgregor+work@gmail.com>
2025-05-07 18:58:22 +0100

    Updated ButtonSectionView to support custom views

--- FringePlanner/ViewDatas/SectionRowData/ButtonSectionView.swift
+++ FringePlanner/ViewDatas/SectionRowData/ButtonSectionView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 /// Displays a button with a title and right pointing arrow
-struct ButtonSectionView: View {
-    let title: String
+struct ButtonSectionView<Content: View>: View {
     let closure: (() -> Void)
+    @ViewBuilder let content: (() -> Content)
     
     var body: some View {
         Button(
@@ -18,10 +18,19 @@ struct ButtonSectionView: View {
             // `NavigationLink` requires a `View` destination otherwise its contents will appear disabled. Including
             // it as the `label` of `Button` allows the UI to be rendered correctly even though the `closure` does
             // not trigger a view.
-            label: { NavigationLink(title, destination: { Text("") }) }
+            label: {
+                NavigationLink(destination: { Text("") }, label: content)
+            }
         )
         // Removes the default button tint which may appear blue in light mode
         .tint(.primary)
+    }
+}
+
+extension ButtonSectionView {
+    /// Helper initializer for simple text-only buttons
+    init(title: String, closure: @escaping () -> Void) where Content == Text {
+        self.init(closure: closure, content: { Text(title) })
     }
 }
 
